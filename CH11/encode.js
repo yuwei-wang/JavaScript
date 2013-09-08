66b262d0e8bdc912b6cd593cb7f80ad255202c63
Yu-Wei Wang <user@fake.ywwang.idv.tw>
2013-09-09 01:37:21 +0800

    20130909-0137
    CH11

---
+++ CH11/encode.js
@@ -0,0 +1,26 @@
+function shiftEncode(inputStr, offset)
+{
+	var encoded, outputStr = "";
+	for (var i = 0; i < inputStr.length; i++)
+	{
+		encoded = inputStr.charCodeAt(i) + offset;
+		outputStr += String.fromCharCode(encoded);
+	}
+	return(outputStr);
+}
+
+function xorEncode(inputStr, keyStr)
+{
+	var outputStr = "";
+	var j = 0;
+	for (var i = 0; i < inputStr.length; i++)
+	{
+		encoded = inputStr.charCodeAt(i) ^ keyStr.charCodeAt(j++);
+		if (j == keyStr.length)
+		{
+			j -= keyStr.length;
+		}
+		outputStr += String.fromCharCode(encoded);
+	}
+	return(outputStr);
+}
